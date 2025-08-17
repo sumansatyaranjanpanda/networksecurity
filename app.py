f7203d08132d874b6eef712d29864e9dfaef6725
Raja Panda <satyaranjanpandasuman@gmail.com>
2025-08-18 01:34:24 +0530

    Deployment changes

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ async def train_route():
         return Response("Training is successful")
     except Exception as e:
         raise NetworkSecurityException(e,sys)
-
+    
 @app.post("/predict")
 async def predict_route(request: Request,file: UploadFile = File(...)):
     try:
@@ -82,9 +82,7 @@ async def predict_route(request: Request,file: UploadFile = File(...)):
         
     except Exception as e:
             raise NetworkSecurityException(e,sys)
-    
-
 
     
 if __name__=="__main__":
-    app_run(app,host="0.0.0.0",port=8080)
+    app_run(app,host="0.0.0.0",port=8000)
